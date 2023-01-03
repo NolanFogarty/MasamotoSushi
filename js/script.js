a0f8e0a229cc0e163a7439a79a278b11abb5a914
NolanFogarty <nolan.k.fogarty@gmail.com>
2023-01-03 15:43:47 -0500

    added Javascript functionality to automatically close nav bar on item click if window is < 992px

---
+++ js/script.js
@@ -0,0 +1,10 @@
+if (window.innerWidth < 992) {
+    const navLinks = document.querySelectorAll('.nav-item:not(.dropdown)')
+    const menuToggle = document.getElementById('navbarSupportedContent')
+    const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false})
+    navLinks.forEach((l) => {
+        l.addEventListener('click', () => {
+            bsCollapse.toggle()
+        })
+    })
+}
