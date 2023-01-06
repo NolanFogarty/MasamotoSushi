739793381bb4262fef95c7da5622a463a44e81f8
NolanFogarty <nolan.k.fogarty@gmail.com>
2023-01-06 06:51:24 -0500

    updated javascript functionality

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-if (window.innerWidth < 992) {
-    const navLinks = document.querySelectorAll('.nav-item:not(.dropdown)')
-    const menuToggle = document.getElementById('navbarSupportedContent')
-    const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false})
-    navLinks.forEach((l) => {
-        l.addEventListener('click', () => {
-            bsCollapse.toggle()
+// Automatically close nav menu after click if page is displayed on smaller device
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.innerWidth < 992) {
+        const navLinks = document.querySelectorAll('.nav-item:not(.dropdown)')
+        const menuToggle = document.getElementById('navbarSupportedContent')
+        const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false})
+        navLinks.forEach((l) => {
+            l.addEventListener('click', () => {
+                bsCollapse.toggle()
+            })
         })
-    })
-}
+    }
+});
+
 
